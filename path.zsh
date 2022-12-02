e6d91f02872526cfb068cc24294c53dd563800e7
Noah Holm <32292420+noppaz@users.noreply.github.com>
2022-12-02 20:12:15 +0100

    use 1Password for SSH agent

--- path.zsh
+++ path.zsh
@@ -1,3 +1,6 @@
+# SSH Agent with 1Password
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
 # Golang paths
 export GOPATH="${HOME}/go"
 export GOROOT="$(brew --prefix golang)/libexec"
