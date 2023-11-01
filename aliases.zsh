eedd6fa95c123ea7abe12c5d83b1087d3b35bcfb
Noah Holm <32292420+noppaz@users.noreply.github.com>
2023-11-01 08:28:48 +0100

    add alias `gsl`

--- aliases.zsh
+++ aliases.zsh
@@ -8,16 +8,17 @@ alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
 
 # Docker
+alias dc="docker compose"
 alias dps="docker ps"
 alias dpsa="docker ps -a"
-alias dc="docker compose"
 
 # Git
 alias commit="git add . && git commit -m"
-alias gs="git status"
 alias gb="git branch"
 alias gc="git checkout"
 alias gd="git diff"
 alias gl="git log --oneline --decorate --color"
 alias gpf="git pull --ff-only"
 alias gpr="git pull --rebase"
+alias gs="git status"
+alias gsl="git stash list | cat"
