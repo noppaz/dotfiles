315a4077c289c8687ae6a807549b07f7b72cf113
Noah Holm <32292420+noppaz@users.noreply.github.com>
2025-10-05 20:45:23 +0200

    remove pyenv and pipenv

--- path.zsh
+++ path.zsh
@@ -5,9 +5,3 @@ export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agen
 export GOPATH="${HOME}/go"
 export GOROOT="$(brew --prefix golang)/libexec"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
-
-# Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PATH:$PYENV_ROOT/bin"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
