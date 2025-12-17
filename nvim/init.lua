76266538e2dd680fd797ff097923df94a0504acf
Noah Holm <32292420+noppaz@users.noreply.github.com>
2025-12-17 21:46:34 +0100

    add ruff lazyvim settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.lsp.config("ruff", {
+  init_options = {
+    settings = {
+      -- Ruff language server settings go here
+    },
+  },
+})
+
+vim.lsp.enable("ruff")
