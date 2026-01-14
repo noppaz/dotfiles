d24cb885bfb4f537750a508186713d844e511fe7
Noah Holm <32292420+noppaz@users.noreply.github.com>
2026-01-14 20:18:18 +0100

    cleanup nvim settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.lsp.config("ruff", {
-  init_options = {
-    settings = {
-      -- Ruff language server settings go here
-    },
-  },
-})
-
-vim.lsp.enable("ruff")
