b56c0da71cbcf963d24146507863bedb8f9c1050
Noah Holm <32292420+noppaz@users.noreply.github.com>
2026-02-14 11:57:00 +0100

    wip[]

---
+++ functions.zsh
@@ -0,0 +1,32 @@
+delete_gh_workflow_runs() {
+  local REPO_WITH_OWNER="$1"
+  local WORKFLOW_NAME="$2"
+
+  if [[ -z "$REPO_WITH_OWNER" || -z "$WORKFLOW_NAME" ]]; then
+    echo "Usage: delete_gh_workflow_runs owner/repo workflow_name.yml"
+    return 1
+  fi
+
+  gh run list -R "$REPO_WITH_OWNER" -w "$WORKFLOW_NAME" -L 100 --json databaseId -q '.[].databaseId'  |
+    xargs -I ID gh run delete -R "$REPO_WITH_OWNER" ID
+}
+
+
+uvpy() {
+  # Symlink a specific uv Python version to /usr/local/bin/python
+  if [ "$#" -ne 1 ]; then
+      echo "Usage: uvpy <python-version>"
+      return 1
+  fi
+
+  PYTHON_VERSION=$1
+  TARGET=$(uv python find --no-project --python-preference only-managed "$PYTHON_VERSION")
+  if [ -z "$TARGET" ]; then
+      echo "Error: Could not find the specified Python version."
+      return 1
+  fi
+
+  LINK_PATH=/usr/local/bin/python
+  echo "Creating symlink: $LINK_PATH -> $TARGET"
+  sudo ln -sf $TARGET $LINK_PATH
+}
