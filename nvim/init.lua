7bd23d31847c170e1244aeabb5e3b29f25bf9bc2
Noah Holm <32292420+noppaz@users.noreply.github.com>
2026-04-28 10:54:01 +0200

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,51 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    }
+},
+{
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = { ':TSUpdate', ':TSInstall! lua' },
+}
+}
+
+local opts = {
+}
+-- lazy
+require("lazy").setup(plugins, opts)
+
+-- theme
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
